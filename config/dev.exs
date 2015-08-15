8d9424603680305017338905a771bf255bb520f4
Victor Campos <victorcampos@ufrj.br>
2015-08-15 07:10:26 -0300

    fixing ecto warning

--- config/dev.exs
+++ config/dev.exs
@@ -32,4 +32,4 @@ config :blog_phoenix, BlogPhoenix.Repo,
   username: "postgres",
   password: "postgres",
   database: "blog_phoenix_dev",
-  size: 10 # The amount of database connections in the pool
+  pool_size: 10 # The amount of database connections in the pool
